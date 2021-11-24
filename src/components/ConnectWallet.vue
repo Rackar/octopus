193839b81e5d7d32d167eba69575066faa4046e3
旭 杨 <27681135@qq.com>
2021-11-24 22:32:03 +0800

    admin game

--- src/components/ConnectWallet.vue
+++ src/components/ConnectWallet.vue
@@ -1,6 +1,23 @@
 <template>
   <span class="right-4 top-2 text-md min-w-max whitespace-nowrap">
     <span
+      v-if="
+        store.state.login &&
+        store.state.address === '0x65D17D3dC59b5ce3d4CE010eB1719882b3f10490'
+      "
+      class="
+        border-success-200 border-solid
+        rounded-md
+        border-2
+        cursor-pointer
+        select-none
+        text-sm
+        p-1
+      "
+      @click="transToGame"
+      >new game contract</span
+    >
+    <span
       v-if="!store.state.login"
       class="
         border-success-200 border-solid
@@ -14,6 +31,7 @@
       @click="connect"
       >connect wallet</span
     >
+
     <span v-else class="text-sm">{{
       store.state.address.substr(0, 5) +
       '...' +
@@ -30,9 +48,14 @@ import { useStore } from 'vuex';
 /*****************************************/
 
 import { requestLoginMetamask } from '../js/web3/index';
+import { transferToGame } from '../js/web3/coinMethods';
 const store = useStore();
 
 async function connect() {
   await requestLoginMetamask();
+}
+
+async function transToGame() {
+  await transferToGame();
 }
 </script>
--- src/js/web3/coinMethods.js
+++ src/js/web3/coinMethods.js
@@ -1,13 +1,18 @@
-import { web3instance } from './getWeb3';
+import Web3 from 'web3';
+// import { web3instance } from './getWeb3';
+import { address_GAME } from '../contract/GAME_ABI';
+import { getInstaceResult } from './index';
+import { weiToCount, countToWei } from './Utils';
 
-//该用户地址有多少币可以提取
-function getLOLICanClaimOf(dnftid, address_User) {
+//给新版本的合约发币
+function transferToGame() {
   return new Promise((resolve, reject) => {
-    web3instance.dloliContract.methods
-      .getLOLICanClaimOf(dnftid, address_User)
-      .call()
+    const ins = getInstaceResult();
+    ins.coinContract.methods
+      .transfer(address_GAME, 10000000000)
+      .send({ from: ins.account })
       .then(result => {
-        console.log(`you have LOLI: ${JSON.stringify(result)}`);
+        console.log(`new contract: ${JSON.stringify(result)}`);
         resolve(result);
       })
       .catch(e => {
@@ -51,9 +56,4 @@ function fetchLOLI(dnftid) {
   });
 }
 
-export {
-  getLOLICanClaimOf,
-  dNFTidToAccountToFetched,
-  fetchLOLI,
-  dnftidCanGetLoli,
-};
+export { transferToGame };
